28679d359c40a66d0fe1932f36f2dd00039cf984
Joshua Froggatt <jfrogg.au@gmail.com>
2024-05-10 21:47:53 +0930

    new file:   A2/AlertCalc.h AlertCalc header file created

---
+++ A2/AlertCalc.h
@@ -0,0 +1,19 @@
+#ifndef AlERT_CALC
+#define ALERT_CALC
+
+#include "AlertCalcStrategy.h"
+
+
+class NoCapSyndrome : public AlertCalcStrategy
+{
+public:
+	void calculateAlert(Patient& patient) override;
+};
+
+class TiccToccBrainDamage : public AlertCalcStrategy
+{
+public:
+	void calculateAlert(Patient& patient) override;
+};
+
+#endif
