e290e7edb9e194d52c5cf00a55d60375d3887f33
Joshua Froggatt <jfrogg.au@gmail.com>
2024-05-28 21:40:59 +0930

    modified:   A2/PatientFileLoader.cpp PatientFileLoader is now converting the data from the txt file into patient files

--- A2/PatientFileLoader.cpp
+++ A2/PatientFileLoader.cpp
@@ -25,46 +25,73 @@ std::vector<Patient*> PatientFileLoader::loadPatientFile()
         std::string birthday;
         std::string lname;
         std::string fname;
-        std::vector<std::string> diagnoses;
-        std::vector<std::string> vitals;
+        std::vector<int> vitals;
         // TODO: load your file here
         while (inFile.peek() != -1)
         {
             std::getline(inFile, uid, '/');
 
-            //converts bday
+            // converts bday
             std::getline(inFile, birthday, '/');
             std::tm t{ };
             std::istringstream ss(birthday);
             ss >> std::get_time(&t, "%d-%m-%Y");
 
-            //last and first name
+            // last and first name
             std::getline(inFile, lname, ',');
             std::getline(inFile, fname, '/');
-            //std::getline(inFile, diagnoses, '/');
 
             Patient* newP = new Patient(fname, lname, t);
 
+
+            // gets the line of diagnoses
             std::string diagnosisLine;
             std::getline(inFile, diagnosisLine, '/');
 
-
-            //converts diagnosis into a stream to stream it into different values for the diagnoses vector
+            // converts diagnosis into a stream to stream it into different values for the diagnoses vector
             std::istringstream diagnosisStream(diagnosisLine);
             std::string diagnosisLS;
             while (std::getline(diagnosisStream, diagnosisLS, ','))
             {
-                diagnoses.push_back(diagnosisLS);
+                newP->addDiagnosis(diagnosisLS);
             }
 
-
-            std::cout << "uid " << uid << std::endl;
-            std::cout << "birthday " << birthday << std::endl;
-            std::cout << "name " << lname << std::endl;
-            std::cout << "name " << fname << std::endl;
-            for (int i = 0; i < diagnoses.size(); i++)
+            // gets the line of vitals
+            std::string vitalLine;
+            std::getline(inFile, vitalLine);
+            
+            // converts vitals into a stream to stream it into different values for the vitals vector
+            std::istringstream vitalsStream(vitalLine);
+            std::string vitalLS;
+            while (std::getline(vitalsStream, vitalLS, ';'))
             {
-                std::cout << "diagnosis " << diagnoses.at(i) << std::endl;
+                // individual vitals
+                std::istringstream indivVitals(vitalLS);
+                std::string vital;
+                while (std::getline(indivVitals, vital, ','))
+                {
+                    int intVital = stoi(vital);
+                    vitals.push_back(intVital);
+                }
+                Vitals* inputVitals =  new Vitals(vitals.at(0), vitals.at(1), vitals.at(2), vitals.at(3));
+                vitals.clear();
+                newP->addVitals(inputVitals);
+            } 
+
+            std::cout << newP->humanReadableID() << std::endl;
+            std::cout << newP->age() << std::endl;
+
+            for (int i = 0; i < newP->diagnoses().size(); i++)
+            {
+                std::cout << newP->diagnoses().at(i) << std::endl;
+            }
+
+            for (int i = 0; i < newP->vitals().size(); i++)
+            {
+                std::cout << "HR  " << newP->vitals().at(i)->HR() << std::endl;
+                std::cout << "SP02  " << newP->vitals().at(i)->SPO2() << std::endl;
+                std::cout << "BT  " << newP->vitals().at(i)->BT() << std::endl;
+                std::cout << "BA  " << newP->vitals().at(i)->BA() << std::endl;
             }
         }
     }
