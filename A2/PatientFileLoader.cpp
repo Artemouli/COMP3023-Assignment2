05c79576355607e9d339d895066612a5d4c6b1db
Joshua Froggatt <jfrogg.au@gmail.com>
2024-05-28 20:38:13 +0930

    modified:   A2/PatientFileLoader.cpp inFile getLine working

--- A2/PatientFileLoader.cpp
+++ A2/PatientFileLoader.cpp
@@ -19,13 +19,28 @@ std::vector<Patient*> PatientFileLoader::loadPatientFile()
     std::vector<Patient*> patients;
 
     std::ifstream inFile(_file);
-    if (inFile.is_open()) {
+    if (inFile.is_open()) 
+    {
+        std::string uid;
+        std::string birthday;
+        std::string name;
+        std::string diagnoses;
+        std::string vitals;
         // TODO: load your file here
-        std::cout << "woah " << std::endl;
-        std::string line;
-        while (std::getline(inFile, line))
+        while (inFile.peek() != -1)
         {
-            std::cout << line << std::endl;
+            std::getline(inFile, uid, '/');
+            std::getline(inFile, birthday, '/');
+            std::getline(inFile, name, '/');
+            std::getline(inFile, diagnoses, '/');
+            std::getline(inFile, vitals);
+
+
+            std::cout << "uid " << uid << std::endl;
+            std::cout << "birthday " << birthday << std::endl;
+            std::cout << "name " << name << std::endl;
+            std::cout << "diagnoses " << diagnoses << std::endl;
+            std::cout << "vitals " << vitals << std::endl;
         }
     }
 
